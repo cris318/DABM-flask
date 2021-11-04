334d49081f88294296077646f146bbefb437bb97
cris318 <cristian.rincon-m@mail.escuelaing.edu.co>
2021-11-03 21:35:41 -0500

    menu principal y archivos estaticos

--- ejemplo.py
+++ ejemplo.py
@@ -1,5 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask.globals import request
+import os
+import random
 
 app = Flask(__name__)
 
@@ -18,14 +20,14 @@ def validar():
         usuario=request.form['usuario']
         password=request.form['password']
 
-        print(usuario)
-        print(password)
-
         resultado=verificar(usuario,password)
+        return render_template('menu.html',title='Sistema DABM')
 
-def verificar():
-    pass
+        #return usuario +";"+ password
 
+def verificar(usuario,password):
+    #usuario no exite,contraseña correcta,bienvenido
+    return True
 
 if __name__=="__main__":
     app.run(debug=True)
