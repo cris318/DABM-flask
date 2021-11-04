7597a05eda87fa54640c67de6bec493261b9aa48
cris318 <cristian.rincon-m@mail.escuelaing.edu.co>
2021-11-03 22:19:58 -0500

    monitor

--- ejemplo.py
+++ ejemplo.py
@@ -24,10 +24,33 @@ def validar():
         return render_template('menu.html',title='Sistema DABM')
 
         #return usuario +";"+ password
-
+@app.route('/monitor')
+def monitor():
+    #consultar archivo de parametros
+    datos=getDatos()
+    #print(datos)
+    # obtener lectura
+    lectura=random.randint(0,45)
+    # enviar a la interfaz
+    return render_template("/monitor.html",datos=datos,lectura=lectura)
+        
 def verificar(usuario,password):
     #usuario no exite,contraseña correcta,bienvenido
     return True
 
+def getDatos():
+    directorio=os.path.dirname(__file__)
+    nombrearchivo="bd/parametros.csv"
+    ruta=os.path.join(directorio,nombrearchivo)
+
+    f= open(ruta,"r")
+    lineas=f.readlines()
+    f.close()
+    datos=[]
+    for l in lineas:
+        l=l.replace("\n","")
+        l=l.split(";")
+        datos.append(l)
+    return datos
 if __name__=="__main__":
     app.run(debug=True)
